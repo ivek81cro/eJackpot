bcc5e21589030a7093b88ecd6e631ad1ab2703f1
ivek81cro <ivek81cro@gmail.com>
2019-07-11 00:30:09 +0200

    izmjene

--- eJackpot/eJackpot.cpp
+++ eJackpot/eJackpot.cpp
@@ -6,16 +6,19 @@
 using namespace std;
 
 int main() {
+	//varijable
 	srand(static_cast<uint32_t>((time(0))));
 	vector<int> c;
-	map<string,int> m;
-	int n, list, kolo=1, stanje;
-	char gen;
-	cout << "How many combinations(tickets) you want to play?"; cin >> list;
 	vector<vector<int>> z;
+	map<string,int> m;
+	int n, list, kolo = 1, stanje = 0;
+	char gen;	
+	//upiti
+	cout << "How many combinations(tickets) you want to play?"; cin >> list;	
 	cout << "How many rounds?"; cin >> n;
 	cout << "Auto generate combinations? (y/n)"; cin >> gen;
 	gen == 'y' ? genList(z, list) : unosList(z, list);
+	//ispis odigranih kombinacija
 	cout << "Your played tickets:" << endl;
 	int x = list;
 	while (x) {
@@ -24,31 +27,37 @@ int main() {
 		cout << endl;
 		--x;
 	}
-	cout << "Please pay " << n * list * 15 << ",00 kn" << endl;
-	cin >> stanje;
-	cout << ">>>>>>>>>>ROLLING<<<<<<<<<<<<<" << endl;
-	while (kolo<=n) {
-		cout << "Round: " << kolo << "-> ";
-		//popuna vektora c(izvlacenje brojeva)
-		izvlacenje(c);
+	//cijena odabranih kombinacija
+	cout << "Please pay " << n * list * 15 << ",00 kn" << endl;	
+	cin >> stanje; 
+	if(stanje< n * list * 15){ cout << "Not enough monney!" << endl; }
+	//ako je sve ok
+	else {
+		cout << ">>>>>>>>>>ROLLING<<<<<<<<<<<<<" << endl;
+		while (kolo <= n) {
+			cout << "Round: " << kolo << "-> ";
+			//popuna vektora c(izvlacenje brojeva)
+			izvlacenje(c);
 
-		//ispis izvucenih brojeva
-		ostream_iterator<int> os(cout, " ");
-		copy(c.begin(), c.end(), os);
-		cout << "-->";
+			//ispis izvucenih brojeva
+			ostream_iterator<int> os(cout, " ");
+			copy(c.begin(), c.end(), os);
+			cout << "-->";
 
-		//provjera dobitaka
-		x = list;
-		while (x) {
-			rez r(cmpV(z[x-1], c));
-			dobitak(r, m);
-			--x;
+			//provjera dobitaka
+			x = list;
+			while (x) {
+				rez r(cmpV(z[x - 1], c));
+				dobitak(r, m);
+				--x;
+			}
+			//priprema za novo kolo
+			cout << endl;
+			c.clear();
+			++kolo;
 		}
-
-		cout << endl;
-		c.clear();
-		++kolo;
 	}
+	//napraviti i novcanu vrijednost dobitaka i usporedbu ulog-gubitak
 	cout << "\nTotal winnings:" << endl;
 	ispis_mape(m);
 }
